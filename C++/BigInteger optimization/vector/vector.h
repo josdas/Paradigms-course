1a1d4d61bb7490bb935d8d4cffdbc6b31c7e4f1e
josdas <josdas@mail.ru>
2017-05-04 09:44:07 +0300

    fix

--- C++/BigInteger optimization/vector/vector.h
+++ C++/BigInteger optimization/vector/vector.h
@@ -10,6 +10,7 @@ struct vector {
 	vector(vector const& a);
 	vector(size_t size, T const& a);
 	explicit vector(size_t size);
+
 	~vector();
 
 	friend bool operator==(vector const& a, vector const& b) {
@@ -23,6 +24,7 @@ struct vector {
 		}
 		return true;
 	}
+
 
 	vector& operator=(vector a);
 
@@ -47,40 +49,27 @@ struct vector {
 
 private:
 	static const size_t size_small = 8;
+
 	size_t _size;
 	bool _is_big;
-
-	T& get_small_ind(size_t ind) {
-		return _obj.small._data[ind];
-	}
-
-	T& get_big_ind(size_t ind) {
-		change();
-		return _obj.big._data.get()[ind];
-	}
-
-	T const& get_small_ind_c(size_t ind) const {
-		return _obj.small._data[ind];
-	}
-
-	T const& get_big_ind_c(size_t ind) const {
-		return _obj.big._data.get()[ind];
-	}
-
-	T& (vector::*function_get)(size_t);
-	T const& (vector::*function_get_const)(size_t) const;
+	T& (vector::*_function_get)(size_t);
+	T const& (vector::*_function_get_const)(size_t) const;
 
 	struct small_big_type { // it can be union
 		struct big_type {
 			std::shared_ptr<T> _data;
 			size_t _capacity;
-			big_type() : _data(nullptr), _capacity(0) { }
 		} big;
 
 		struct small_type {
 			T _data[size_small];
 		} small;
 	} _obj;
+
+	T& get_small_ind(size_t ind);
+	T& get_big_ind(size_t ind);
+	T const& get_small_ind_c(size_t ind) const;
+	T const& get_big_ind_c(size_t ind) const;
 
 	static size_t get_new_capacity(size_t n);
 	void set_capacity(size_t capacity);
@@ -89,22 +78,25 @@ private:
 	void change();
 
 	bool is_big() const;
-	void transform_to_big(size_t n);
+	void set_big_option();
+
+	size_t get_capacity();
+	T* get_data();
 };
 
 template <class T>
 vector<T>::vector() :
 	_size(0),
 	_is_big(false),
-	function_get(&vector<T>::get_small_ind),
-	function_get_const(&vector<T>::get_small_ind_c) { }
+	_function_get(&vector<T>::get_small_ind),
+	_function_get_const(&vector<T>::get_small_ind_c) { }
 
 template <class T>
 vector<T>::vector(vector const& a) :
 	_size(a.size()),
 	_is_big(a._is_big),
-	function_get(a.function_get),
-	function_get_const(a.function_get_const),
+	_function_get(a._function_get),
+	_function_get_const(a._function_get_const),
 	_obj(a._obj) { }
 
 template <class T>
@@ -131,12 +123,12 @@ vector<T>& vector<T>::operator=(vector a) {
 
 template <class T>
 T& vector<T>::operator[](size_t ind) {
-	return (this ->* function_get)(ind);
+	return (this ->* _function_get)(ind);
 }
 
 template <class T>
 T const& vector<T>::operator[](size_t ind) const {
-	return (this ->* function_get_const)(ind);
+	return (this ->* _function_get_const)(ind);
 }
 
 template <class T>
@@ -173,39 +165,26 @@ size_t vector<T>::size() const {
 
 template <class T>
 void vector<T>::reserve(size_t n) {
-	if (is_big()) {
-		change();
-		if (_obj.big._capacity < n) {
-			set_capacity(get_new_capacity(n));
-		}
-	}
-	else {
-		if (n > size_small) {
-			transform_to_big(n);
-		}
+	if (get_capacity() < n) {
+		set_capacity(get_new_capacity(n));
 	}
 }
 
 template <class T>
 void vector<T>::push_back(T const& value) {
-	if (is_big()) {
-		change();
-		if (size() == _obj.big._capacity) {
-			size_t capacity = get_new_capacity(size() + 1);
-			T* temp = copy_data(capacity, _obj.big._data.get(), size());
-			new(&temp[size()]) T(value);
-			delete_all();
-			_obj.big._data.reset(temp);
-			_obj.big._capacity = capacity;
-		}
-		else {
-			new(&(*this)[size()]) T(value);
+	if (size() == get_capacity()) {
+		size_t capacity = get_new_capacity(size() + 1);
+		T* temp = copy_data(capacity, get_data(), size());
+		new(&temp[size()]) T(value);
+		delete_all();
+		_obj.big._data.reset(temp);
+		_obj.big._capacity = capacity;
+		if (!is_big()) {
+			set_big_option();
 		}
 	}
 	else {
-		if (size() == size_small) {
-			transform_to_big(size() + 1);
-		}
+		change();
 		new(&(*this)[size()]) T(value);
 	}
 	_size++;
@@ -215,11 +194,8 @@ template <class T>
 void vector<T>::pop_back() {
 	if (is_big()) {
 		change();
-		_obj.big._data.get()[size() - 1].~T();
 	}
-	else {
-		_obj.small._data[size() - 1].~T();
-	}
+	(*this)[size()].~T();
 	_size--;
 }
 
@@ -229,48 +205,60 @@ void vector<T>::swap(vector& a) noexcept {
 	swap(a._obj, _obj);
 	swap(a._size, _size);
 	swap(a._is_big, _is_big);
-	swap(a.function_get, function_get);
-	swap(a.function_get_const, function_get_const);
+	swap(a._function_get, _function_get);
+	swap(a._function_get_const, _function_get_const);
+}
+
+template <class T>
+T& vector<T>::get_small_ind(size_t ind) {
+	return _obj.small._data[ind];
+}
+
+template <class T>
+T& vector<T>::get_big_ind(size_t ind) {
+	change();
+	return _obj.big._data.get()[ind];
+}
+
+template <class T>
+T const& vector<T>::get_small_ind_c(size_t ind) const {
+	return _obj.small._data[ind];
+}
+
+template <class T>
+T const& vector<T>::get_big_ind_c(size_t ind) const {
+	return _obj.big._data.get()[ind];
 }
 
 template <class T>
 size_t vector<T>::get_new_capacity(size_t n) {
 	if (n == 0) {
-		return 8;
+		return 4;
 	}
 	return n * 3 / 2;
 }
 
 template <class T>
 void vector<T>::set_capacity(size_t capacity) {
-	if (is_big()) {
-		T* temp = copy_data(capacity, _obj.big._data.get(), size());
+	if (is_big() || capacity > size_small) {
+		T* temp = copy_data(capacity, get_data(), size());
 		delete_all();
 		_obj.big._data.reset(temp);
 		_obj.big._capacity = capacity;
-	}
-	else {
-		if (capacity > size_small) {
-			transform_to_big(capacity);
+		if(!is_big()) {
+			set_big_option();
 		}
 	}
 }
 
 template <class T>
 void vector<T>::delete_all() {
-	if (is_big()) {
-		if (_obj.big._capacity > 0) {
-			delete_data(_obj.big._data.get(), size());
-		}
-	}
-	else {
-		delete_data(_obj.small._data, size());
-	}
+	delete_data(get_data(), size());
 }
 
 template <class T>
 void vector<T>::change() {
-	if (!_obj.big._data.unique()) {
+	if (is_big() && !_obj.big._data.unique()) {
 		_obj.big._data.reset(copy_data(size(), _obj.big._data.get(), size()));
 	}
 }
@@ -281,19 +269,31 @@ bool vector<T>::is_big() const {
 }
 
 template <class T>
-void vector<T>::transform_to_big(size_t n) {
+void vector<T>::set_big_option() {
 	_is_big = true;
-	function_get = &vector<T>::get_big_ind;
-	function_get_const = &vector<T>::get_big_ind_c;
-	T* temp = copy_data(n, _obj.small._data, size());
-	delete_data(_obj.small._data, size());
-	_obj.big._data.reset(temp);
-	_obj.big._capacity = n;
+	_function_get = &vector<T>::get_big_ind;
+	_function_get_const = &vector<T>::get_big_ind_c;
+}
+
+template <class T>
+size_t vector<T>::get_capacity() {
+	if (is_big()) {
+		return _obj.big._capacity;
+	}
+	return size_small;
+}
+
+template <class T>
+T* vector<T>::get_data() {
+	if (is_big()) {
+		return _obj.big._data.get();
+	}
+	return _obj.small._data;
 }
 
 template <class T>
 T* copy_data(size_t capacity, T* data, size_t size,
-             typename std::enable_if<!std::is_trivially_constructible<T>::value>::type* = nullptr) {
+		typename std::enable_if<!std::is_trivially_constructible<T>::value>::type* = nullptr) {
 	T* temp = static_cast<T*>(operator new(capacity * sizeof(T)));
 	for (size_t i = 0; i < size; i++) {
 		try {
@@ -310,7 +310,7 @@ T* copy_data(size_t capacity, T* data, size_t size,
 
 template <class T>
 T* copy_data(size_t capacity, T* data, size_t size,
-             typename std::enable_if<std::is_trivially_constructible<T>::value>::type* = nullptr) {
+		typename std::enable_if<std::is_trivially_constructible<T>::value>::type* = nullptr) {
 	T* temp = static_cast<T*>(operator new(capacity * sizeof(T)));
 	memcpy(temp, data, sizeof(T) * size);
 	return temp;
@@ -318,7 +318,7 @@ T* copy_data(size_t capacity, T* data, size_t size,
 
 template <class T>
 void delete_data(T* data, size_t size,
-                 typename std::enable_if<!std::is_trivially_destructible<T>::value>::type* = nullptr) {
+		typename std::enable_if<!std::is_trivially_destructible<T>::value>::type* = nullptr) {
 	for (size_t j = 0; j < size; j++) {
 		data[j].~T();
 	}
@@ -326,5 +326,5 @@ void delete_data(T* data, size_t size,
 
 template <class T>
 void delete_data(T* data, size_t size,
-                 typename std::enable_if<std::is_trivially_destructible<T>::value>::type* = nullptr) { }
+		typename std::enable_if<std::is_trivially_destructible<T>::value>::type* = nullptr) { }
 #endif // VECTOR
